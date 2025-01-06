eac951a40ad965d245c007d57eeb29c67e025d19
PhanAnhTuan123 <phantuan7a5@gmail.com>
2025-01-06 08:34:08 +0700

    feat:Update file for infra

---
+++ gateway-service/src/main/java/com/eMartix/gateway_service/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.eMartix.gateway_service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(issuerUri).build();
+    }
+
+//    @Bean
+//    public AuthenticationExceptionHand
+
+}
