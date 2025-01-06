fa952a5754d1be6e9e6dcddd12b2ebf09f37ea2d
PhanAnhTuan123 <phantuan7a5@gmail.com>
2025-01-06 07:54:12 +0700

    feat: Update registry service with eureka

---
+++ service-registry/src/test/java/com/eMartix/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eMartix.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
