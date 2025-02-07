159a54e6778f1ae706386bd2f3388d6d0227d3ae
PhanAnhTuan123 <phantuan7a5@gmail.com>
2025-02-07 22:55:35 +0700

    chore: Fix @annotation

--- commons/src/main/java/com/eMartix/commons/id/NanoIDGenerator.java
+++ commons/src/main/java/com/eMartix/commons/id/NanoIDGenerator.java
@@ -1,11 +1,20 @@
 package com.eMartix.commons.id;
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
 import jakarta.persistence.PrePersist;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
 
-public class NanoIDGenerator {
-    @PrePersist
-    public String generate() {
-        return NanoIdUtils.randomNanoId(NanoIdUtils.DEFAULT_NUMBER_GENERATOR,
-                NanoIdUtils.DEFAULT_ALPHABET,21);
+import java.security.SecureRandom;
+
+public class NanoIDGenerator implements IdentifierGenerator {
+    
+    private static final SecureRandom RANDOM = new SecureRandom();
+    private static final char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+    private static final int ID_LENGTH = 16; // Adjust length as needed
+
+    @Override
+    public Object generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object object) {
+        return NanoIdUtils.randomNanoId(RANDOM, ALPHABET, ID_LENGTH);
     }
+
 }
